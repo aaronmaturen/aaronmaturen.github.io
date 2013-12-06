d029ac724e105f72b9148f2ce42489038fd1366d
Aaron T. Maturen <atmature@svsu.edu>
2013-12-06 10:43:20 -0500

    the doughnut was firing multiple times

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,19 +1,22 @@
 var showDoughnutChart = function(element){
     var $element = $(element);
-    var doughnutData = [{
-            value: $element.data('level'),
-            color:"#1abc9c"
-        },
-        {
-            value : 100 - Number($element.data('level')),
-            color : "#ecf0f1"
-        }];
-
-    var myDoughnut = new Chart(element.getContext("2d")).Doughnut(doughnutData, {
-        percentageInnerCutout : 65,
-        animationEasing : "easeInOutSine",
-        animateRotate : true
-    });
+    if(!$element.data('triggered')){
+        var doughnutData = [{
+                value: $element.data('level'),
+                color:"#1abc9c"
+            },
+            {
+                value : 100 - Number($element.data('level')),
+                color : "#ecf0f1"
+            }];
+    
+        var myDoughnut = new Chart(element.getContext("2d")).Doughnut(doughnutData, {
+            percentageInnerCutout : 65,
+            animationEasing : "easeInOutSine",
+            animateRotate : true
+        });
+        $element.data('triggered', true)
+    }
 };
 
 $(window).load(function() {
